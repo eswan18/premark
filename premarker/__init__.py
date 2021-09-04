18268fff701fab2b11f5a2580b23d8107fc9e97a
Ethan Swan <eswan18@rocketmail.com>
2021-09-04 17:37:30 -0500

    Rename to premarker.

--- premarker/__init__.py
+++ premarker/__init__.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from ._version import get_versions
+from ._version import __version__ as version
 
 from . import presentation
 from .presentation import Presentation
 
-__author__ = "Dave Forgac"
-__email__ = "tylerdave@tylerdave.com"
-__version__ = get_versions()["version"]
-del get_versions
+__author__ = "Dave Forgac, Ethan Swan"
+__email__ = "eswan18@rocketmail.com"
+__version__ = version
 
 
 __all__ = ["presentation", "Presentation", "__author__", "__email__", "__version__"]
