4172d19e56a9fd2f2b763888b4b32f7cfa6910b7
Ethan Swan <eswan18@rocketmail.com>
2021-09-05 10:37:44 -0500

    Get versioning working.

--- premarker/__init__.py
+++ premarker/__init__.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-from ._version import __version__ as version
 
 from . import presentation
 from .presentation import Presentation
 
 __author__ = "Dave Forgac, Ethan Swan"
 __email__ = "eswan18@rocketmail.com"
-__version__ = version
+__version__ = '0.0.9'
 
 
 __all__ = ["presentation", "Presentation", "__author__", "__email__", "__version__"]
