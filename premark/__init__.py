5c9b1ac83a06cae5132c5e9748b42b59ab6f38be
Ethan Swan <eswan18@rocketmail.com>
2021-09-13 20:19:59 -0500

    Switch to version 0.0.9rc0.

--- premark/__init__.py
+++ premark/__init__.py
@@ -6,7 +6,7 @@ from .presentation import Presentation
 
 __author__ = "Dave Forgac, Ethan Swan"
 __email__ = "eswan18@rocketmail.com"
-__version__ = '0.0.9'
+__version__ = '0.0.9rc0'
 
 
 __all__ = ["presentation", "Presentation", "__author__", "__email__", "__version__"]
