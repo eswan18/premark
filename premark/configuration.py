2b24e5e0806866de5c707c09e8315c100face8f0
Ethan Swan <eswan18@rocketmail.com>
2022-04-26 10:55:11 -0500

    Implement full Mapping protocol on PartialConfig.

--- premark/configuration.py
+++ premark/configuration.py
@@ -6,7 +6,10 @@ from pkg_resources import resource_filename
 import yaml
 
 
-P = TypeVar('P', bound='_PartialConfig')
+P = TypeVar('P', bound='PartialConfig')
+
+def pkg_file(path: str) -> str:
+    return resource_filename('premark', path)
 
 
 @runtime_checkable
@@ -14,13 +17,26 @@ class Readable(Protocol):
     def read(self) -> Union[str, bytes]: ...
 
 
-class _PartialConfig:
+logger = logging.getLogger(__name__)
+
+
+class PartialConfig:
 
     def __init__(
         self,
         config: Mapping[str, Any],
     ):
-        self._config_map = config
+        def sub_pkg_name(s: str) -> Any:
+            '''Replace {{premark}} with the package path.'''
+            if isinstance(s, str) and s.startswith('{{premark}}'):
+                updated = pkg_file(s.replace('{{premark}}', ''))
+                logger.debug('Replacing config value %s with %s', s, updated)
+                return updated
+            return s
+        self._config_map = {
+            key: sub_pkg_name(value) for key, value in config.items()
+        }
+        logger.debug('Created new PartialConfig with keys %s', self.keys())
 
     @classmethod
     def from_file(
@@ -50,11 +66,17 @@ class _PartialConfig:
         conf = yaml.load(_yaml, Loader=yaml.SafeLoader)
         return cls(conf)
 
+    def items(self) -> Iterator[str, Any]:
+        return self._config_map.items()
+
+    def keys(self) -> Iterator[str]:
+        return self._config_map.keys()
+
+    def values(self) -> Iterator[Any]:
+        return self._config_map.values()
+
     def __iter__(self) -> Iterator[str]:
         return iter(self._config_map)
 
     def __getitem__(self, key: str) -> Any:
         return self._config_map[key]
-
-
-default_config = _PartialConfig.from_file(resource_filename('premark', 'config.yaml'))
