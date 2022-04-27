edbbe8131c6d11c11d8510eebf8a5dde2f2a2c81
Ethan Swan <eswan18@rocketmail.com>
2022-04-27 12:02:39 -0600

    Add some more tests.

--- tests/unit/test_partial_config.py
+++ tests/unit/test_partial_config.py
@@ -1,6 +1,12 @@
 from collections import ChainMap
+from pathlib import Path
 
+import pytest
+from yaml.error import YAMLError
+
+import premark
 from premark.config import PartialConfig
+
 
 def test_can_be_used_in_chain_map():
     '''
@@ -15,13 +21,31 @@ def test_can_be_used_in_chain_map():
     assert chain['b'] == 4
     assert chain['d'] == 8
 
-def test_from_file(tmp_path):
+def test_from_file(tmp_path: Path):
     '''
     Reading from a file works as expected.
     '''
     filepath = tmp_path / 'config.yaml'
     filepath.write_text('remark_args:\n  ratio: 3\n  stuff: 7\nother: a')
-    c = PartialConfig.from_file(filepath)
+    p = PartialConfig.from_file(filepath)
 
-    assert c['other'] == 'a'
-    assert c['remark_args']['ratio'] == 3
+    assert p['other'] == 'a'
+    assert p['remark_args']['ratio'] == 3
+
+def test_from_file_fails_on_bad_yaml(tmp_path: Path):
+    '''
+    Invalid yaml triggers an error.
+    '''
+    filepath = tmp_path / 'config.yaml'
+    filepath.write_text('remark_args:  ratio: 3\n  stuff: 7\nother: a')
+    with pytest.raises(YAMLError):
+        p = PartialConfig.from_file(filepath)
+
+def test_interpolates_pkg_path():
+    '''
+    Configs can use {{premark}} as a shorthand for the path to the package.
+    '''
+    p = PartialConfig({'a': 1, 'fakefile': '{{premark}}/fakefile.py'})
+    expected_path = Path(premark.__file__).parent / 'fakefile.py'
+    assert p['fakefile'] == str(expected_path)
+    
