55b6b8b13901b1276e670e0a172db9eece33a0a0
Ethan Swan <eswan18@rocketmail.com>
2022-04-27 11:47:56 -0600

    Add another test.

--- tests/unit/test_partial_config.py
+++ tests/unit/test_partial_config.py
@@ -3,6 +3,9 @@ from collections import ChainMap
 from premark.config import PartialConfig
 
 def test_can_be_used_in_chain_map():
+    '''
+    PartialConfigs work as mappings and can be used in a ChainMap
+    '''
     p1 = PartialConfig({'a': 1})
     p2 = PartialConfig({'a': 2, 'b': 4, 'c': 7})
     p3 = PartialConfig({'b': 6, 'd': 8})
@@ -11,3 +14,14 @@ def test_can_be_used_in_chain_map():
     assert chain['a'] == 1
     assert chain['b'] == 4
     assert chain['d'] == 8
+
+def test_from_file(tmp_path):
+    '''
+    Reading from a file works as expected.
+    '''
+    filepath = tmp_path / 'config.yaml'
+    filepath.write_text('remark_args:\n  ratio: 3\n  stuff: 7\nother: a')
+    c = PartialConfig.from_file(filepath)
+
+    assert c['other'] == 'a'
+    assert c['remark_args']['ratio'] == 3
