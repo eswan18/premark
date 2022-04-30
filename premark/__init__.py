b44ecf90110f2ab898c8c70da49419eec903b9fd
Ethan Swan <eswan18@rocketmail.com>
2022-04-30 15:13:12 -0600

    Update my email somewhere else.

--- premark/__init__.py
+++ premark/__init__.py
@@ -3,8 +3,8 @@ from . import presentation
 from .presentation import Presentation
 
 __author__ = "Dave Forgac, Ethan Swan"
-__email__ = "eswan18@rocketmail.com"
-__version__ = '0.0.9'
+__email__ = "ethanpswan@gmail.com"
+__version__ = '0.9.0b1'
 
 
 __all__ = ["presentation", "Presentation", "__author__", "__email__", "__version__"]
