c539e929530f40b57ce9c93f2494292c1f190e23
Ethan Swan <eswan18@rocketmail.com>
2022-05-01 11:46:56 -0600

    Update copyright to 2022.

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Premark'
-copyright = '2021, Ethan Swan'
+copyright = '2022, Ethan Swan'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
