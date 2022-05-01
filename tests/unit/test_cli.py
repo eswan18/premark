3b6c3a2a90fb716bdbfd7d82bce9f6124b2d7c84
Ethan Swan <eswan18@rocketmail.com>
2022-04-30 20:25:30 -0600

    Add more complex CLI unit test.

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -11,6 +11,20 @@ def runner():
     return CliRunner()
 
 
+@pytest.fixture
+def PrezMock():
+    '''
+    Mock Presentation class.
+    '''
+    class _PrezMock(MagicMock):
+        __init__ = MagicMock(return_value=None)
+
+        def to_html(self):
+            return 'fake output'
+
+    return _PrezMock
+
+
 def test_shows_help(runner):
     result = runner.invoke(cli.premark, [])
     assert result.exit_code == 2
@@ -18,17 +32,14 @@ def test_shows_help(runner):
     assert "Error: Missing argument" in result.output
 
 
-def test_simple_invocation(runner, mocker):
-    class PrezMock(MagicMock):
-        __init__ = MagicMock(return_value=None)
-
-        def to_html(self):
-            return 'fake output'
-
+def test_simple_invocation(runner, mocker, PrezMock):
+    '''
+    A simple call to the CLI passes the expected args to Presentation.
+    '''
     mocker.patch('premark.cli.Presentation', PrezMock)
-
     source_file = 'slides.md'
     out_file = 'slides.html'
+
     with runner.isolated_filesystem():
         with open(source_file, 'wt') as f:
             f.write('# Slide 1\n---')
@@ -44,4 +55,42 @@ def test_simple_invocation(runner, mocker):
         stylesheet=None,
         title=None,
         config_file=None,
+    )
+
+
+def test_full_invocation(runner, mocker, PrezMock):
+    '''
+    A complex call to the CLI passes the expected args to Presentation.
+    '''
+    mocker.patch('premark.cli.Presentation', PrezMock)
+    source_file = 'slides.md'
+    html_file = 'template.html'
+    css_file = 'styles.css'
+    config_file = 'config.yaml'
+    out_file = 'slides.html'
+    title = 'My Prez'
+
+    with runner.isolated_filesystem():
+        for filename in (source_file, html_file, css_file, config_file):
+            with open(filename, 'wt') as f:
+                f.write('Nonsense')
+        result = runner.invoke(cli.premark, [
+            '--outfile', out_file,
+            '--html', html_file,
+            '--stylesheet', css_file,
+            '--title', title,
+            '--config', config_file,
+            source_file
+        ])
+        # The output file should contain the result of to_html()
+        with open(out_file, 'rt') as f:
+            assert f.read() == 'fake output'
+
+    assert result.exit_code == 0
+    PrezMock.__init__.assert_called_once_with(
+        source_file,
+        html_template=html_file,
+        stylesheet=css_file,
+        title=title,
+        config_file=config_file,
     )
