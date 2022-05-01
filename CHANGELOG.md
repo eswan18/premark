9d9705e5506ca058989b2aa9099aa6e59dcc43da
Ethan Swan <eswan18@rocketmail.com>
2022-05-01 11:33:58 -0600

    Go to version 0.1.0; update changelog.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## Version 0.1.0
+
+- Overhaul interface of both the CLI and the `Presentation` class.
+- Support config files as yaml.
+
 ## Version 0.0.9
 
 - First release
--- premark/__init__.py
+++ premark/__init__.py
@@ -4,7 +4,7 @@ from .presentation import Presentation
 
 __author__ = "Dave Forgac, Ethan Swan"
 __email__ = "ethanpswan@gmail.com"
-__version__ = '0.9.0b1'
+__version__ = '0.1.0'
 
 
 __all__ = ["presentation", "Presentation", "__author__", "__email__", "__version__"]
